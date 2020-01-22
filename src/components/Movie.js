4a21a2375fc4da6df18aa6a1853b5d03ff495279
Ciaran <ciarangrimshawdev@gmail.com>
2020-01-22 22:49:55 +0000

    Fetching movie data from API using hook

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,16 +7,22 @@ import Actor from './elements/Actor';
 import Grid from './elements/Grid';
 import Spinner from './elements/Spinner';
 
-const Movie = ({ movieId }) => (
-  <>
-    <Navigation />
-    <MovieInfo />
-    <MovieInfoBar />
-    <Grid>
-      <Actor />
-    </Grid>
-    <Spinner />
-  </>
-);  
+import { useMovieFetch } from './hooks/useMovieFetch';
+
+const Movie = ({ movieId }) => {
+  const [movie, loading, error] = useMovieFetch(movieId);
+  
+  return (
+    <>
+      <Navigation />
+      <MovieInfo />
+      <MovieInfoBar />
+      <Grid>
+        <Actor />
+      </Grid>
+      <Spinner />
+    </>
+  );
+}
 
 export default Movie;
