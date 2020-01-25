28ea18f9a21e6ae02354acbd3468b29b0d7a6953
Ciaran <ciarangrimshawdev@gmail.com>
2020-01-25 17:13:45 +0000

    Add build folder

---
+++ build/precache-manifest.d88b8d7dd68c9e57fd0696e3cd318285.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb94a8a44bb86bb962a235a4f38eb16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1acdf6f5254370edcd9",
+    "url": "/static/js/2.ad1fe60f.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.ad1fe60f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28e01dbdeef1e2c8877f",
+    "url": "/static/js/main.1614d099.chunk.js"
+  },
+  {
+    "revision": "e4f325194e54e25e704a",
+    "url": "/static/js/runtime-main.3c491689.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/static/media/reactMovie_logo.08494abf.png"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
